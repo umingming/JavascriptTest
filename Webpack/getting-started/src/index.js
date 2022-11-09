2e63a7710650a4a7f9a933d734f050c4c429a95f
umingming <u_0112@naver.com>
2022-11-09 11:25:48 +0900

    Add component for lodash

--- Webpack/getting-started/src/index.js
+++ Webpack/getting-started/src/index.js
@@ -1,10 +1,11 @@
-import _ from 'lodash'; 
+// import _ from 'lodash'; 
 // package.json 에서 설정함.
 
 function component() {
     var element = document.createElement('div');
 
     element.innerHTML = _.join(['Hello', 'Webpack'], ' ');
+    // _가 lodash고, 여기서 join이라는 API 사용함.
     return element;
 }
 
