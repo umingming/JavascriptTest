246d4bb26b74608b0a007bae22bcad556767046d
umingming <u_0112@naver.com>
2022-11-16 16:20:40 +0900

    Add param log

--- vo/easyVO.js
+++ vo/easyVO.js
@@ -1,17 +1,67 @@
-let ddl = '"WARNINGTIMEDURATION" NUMBER, "QUEUETIMEDURATION" NUMBER, "WARNINGID" VARCHAR2(40), "ALARMID" VARCHAR2(40), "CREATETIME" DATE, "CREATEUSER" VARCHAR2(40), "LASTMODIFIEDTIME" DATE, "LASTMODIFIEDUSER" VARCHAR2(40), "TOPODESC" VARCHAR2(250), "RUNCARDID" VARCHAR2(80), "RUNCARDRELFLAG" VARCHAR2(10), "MAXQUEUETIME" VARCHAR2(40), "TOPROCESSOPERATIONVERSION" VARCHAR2(40), "TOPROCESSOPERATIONNAME" VARCHAR2(40),';
-let colInfoList = ddl.split(",");
+let ddl = `POSQueueTime	conditionId	String	Y
+POSQueueTime	channelId	String	Y
+POSQueueTime	fromEventName	String	N
+POSQueueTime	toMainPfName	String	N
+POSQueueTime	toMainPfVersion	String	N
+POSQueueTime	toStepSequence	String	N
+POSQueueTime	toPoName	String	N
+POSQueueTime	toEventName	String	N
+POSQueueTime	queueType	String	N
+POSQueueTime	warningTimeDuration	Long	N
+POSQueueTime	queueTimeDuration	Long	N
+POSQueueTime	warningId	String	N
+POSQueueTime	alarmId	String	N
+POSQueueTime	createTime	TimeStamp	N
+POSQueueTime	createUser	String	N
+POSQueueTime	lastModifiedTime	TimeStamp	N
+POSQueueTime	lastModifiedUser	String	N`
+let colInfoList = ddl.split(`
+`);
+let pkList = [];
+let paramList = [];
 
 function getVariableStr(info) {
-    let name = info.split(" ")[0].replace(/"/g, '');
-    let type = info.split(" ")[1];
-    switch(type) {
-        case 'NUMBER':
-            return `public Long ${name}`;
-        case 'DATE':
-            return `public Date ${name}`;
-        default :
-            return `public String ${name}`;
-    }
+    let name = info.split("	")[1];
+    let type = info.split("	")[2].replace("TimeStamp", "Date").replace("Double", "String");
+
+    return `public ${type} ${name};`;
 }
 
-colInfoList.forEach(i => console.log(getVariableStr(i.trim())));
+colInfoList.forEach(i => {
+    let pkFlag = i.split("	")[3];
+    if (pkFlag === "Y") pkList.push(i)
+    console.log(getVariableStr(i))
+});
+
+console.log();
+pkList.forEach(i => {
+    let name = i.split("	")[1];
+    let type = i.split("	")[2].replace("TimeStamp", "Date").replace("Double", "String");
+    paramList.push(` ${type} ${name}`);
+    console.log(`this.${name} = s${name.replace(/^[a-z]/, char => char.toUpperCase())};`);
+});
+
+console.log();
+console.log(paramList.toString().trim());
+
+
+// const readline = require("readline");
+
+// const rl = readline.createInterface({
+//     input: process.stdin,
+//     output: process.stdout,
+// });
+
+// let input = [];
+
+// rl.on("line", (line) => {
+//     input.push(line)
+//     rl.close();
+// });
+
+// rl.on("close", () => {
+//     console.log(input);
+//     process.exit();
+// })
+
+
