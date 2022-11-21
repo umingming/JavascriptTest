aab56acd8f8df6d1d2ceb6f49bff075fb8c3b496
umingming <u_0112@naver.com>
2022-11-21 13:08:32 +0900

    Commit

---
+++ Webpack/code-splitting/webpack.config.js
@@ -0,0 +1,16 @@
+var path = require('path');
+
+module.exports = {
+	mode: 'none',
+	entry: './index.js', 
+	output: {
+		filename: 'bundle.js',
+		path: path.resolve(__dirname, 'dist')
+	},
+	module: {
+		rules: [{
+			test: /\.css$/,
+			use: ['style-loader', 'css-loader']
+		}]
+	}
+};
