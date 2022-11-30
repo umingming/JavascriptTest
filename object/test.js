ff944362218f8b6c0bbd3b6ce037ae2c26da124e
umingming <u_0112@naver.com>
2022-11-30 11:11:03 +0900

    Test getter and setter

--- object/test.js
+++ object/test.js
@@ -3,8 +3,30 @@ arr.top = function () {
     return arr[arr.length - 1];
 };
 
-console.log(arr.top());
+let num = [10, 20, 30];
+// console.log(arr.top());
 
 // function top() {
 //     return this[this.length - 1];
 // }
+
+function getInfo(name) {
+    return {
+        AGE: num[10],
+        NUM: num,
+        get NUM() {
+            return num;
+        },
+        set NUM(arr) {
+            num = arr;
+        }
+    }
+}
+
+let num2 = getInfo("유미");
+console.log(num2.NUM);
+num2.NUM = [1, 2, 3]
+
+console.log(num2.NUM);
+console.log(getInfo("유미").NUM);
+console.log(num);
