a218709d3427752ede7336ca91dee9710c3da001
umingming <u_0112@naver.com>
2022-12-02 11:04:00 +0900

    즉시 실행 함수

---
+++ scope/14-07.js
@@ -0,0 +1,15 @@
+var Counter = (function () {
+    var num = 0;
+    return {
+        increase() {
+            return ++num;
+        },
+        decrease() {
+            return --num;
+        }
+    };
+}());
+
+console.log(Counter.num);
+console.log(Counter.increase());
+console.log(Counter.increase());
