80064e297f30d97a061bcd814448c13abb5e291a
umingming <u_0112@naver.com>
2022-12-14 15:35:06 +0900

    Set prototype to user setPrototypeOf

--- prototype/example.js
+++ prototype/example.js
@@ -1,4 +1,10 @@
 const parent = {};
 const child = {};
 child.__proto__ = parent;
-parent.__proto__ = child;
+
+const obj = Object.create(null);
+console.log(obj.__proto__);
+console.log(Object.getPrototypeOf(obj));
+
+Object.setPrototypeOf(obj, parent);
+console.log(Object.getPrototypeOf(obj));
