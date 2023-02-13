2da370e6157c6538fa6e57b6e3801306f0339a56
umingming <u_0112@naver.com>
2023-02-13 13:29:54 +0900

    parse string to int

---
+++ builtIn/parseInt.js
@@ -0,0 +1,8 @@
+// console.log(parseInt(10));
+// console.log(parseInt(' 100 '));
+// console.log(parseInt(' 100 years'));
+// console.log(parseInt('im 100 years'));
+// console.log(parseInt('111', 2));
+
+const x = parseInt('111', 2);
+console.log(x.toString(2));
