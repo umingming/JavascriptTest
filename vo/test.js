d65dc949f5138ed6082504c19dd410e94b7aaf69
umingming <u_0112@naver.com>
2023-04-05 15:51:43 +0900

    feat: Object to string

---
+++ vo/test.js
@@ -0,0 +1,529 @@
+const obj = {
+    "appHeader": {
+        "welcome":"welcome",
+        "title":"FactoryModeler",
+        "modalLogout": {
+            "headerTitle": "Logout",
+            "bodyTitle1": "要退出吗？",
+            "bodyTitle2": "Log out?",
+            "bodyContent": "移动到登录界面。",
+            "footerButtonClose": "Close",
+            "footerButtonOk": "Ok"
+        },
+        "modalSetDefault": {
+            "factory": "Factory",
+            "area": "Area",
+            "bay": "Bay"
+        }
+    },
+    "appFooter": {
+        "history": "History",
+        "navigation": "Navigation",
+        "memo": "Memo",
+        "favorite": "Favorite"
+    },
+    "main": {
+        "navigation": "Flow Navigation",
+        "favorite": "Favorite Menu",
+        "memoModify": "修改Memo",
+        "writeMemo": "请做简要记录..."
+    },
+    "designer": {
+        "title":"ProcessFlowDesigner",
+        "flow":"ProcessFlow",
+        "operation":"ProcessOperation",
+        "viewer":"Viewer",
+        "showVersion":"Show Version",
+        "flowName": "ProcessFlowName",
+        "flowVersion": "ProcessFlowVersion",
+        "operationName": "ProcessOperationName",
+        "operationVersion": "ProcessOperationVersion",
+        "nodeId":"NodeID",
+        "nodeCondition":"NodeCondition",
+        "function":"Function",
+        "objectType":"ObjectType",
+        "fieldInfo":"FieldInfo",
+        "checkType":"CheckType",
+        "operator":"Operator",
+        "value":"Value",
+        "expression":"Expression",
+        "nodeTab": "Node Tab",
+        "releaseRework": "Release Rework",
+        "nodeTag": "Node Tag",
+        "propertiesPanel":"Properties Panel",
+        "wait":"Waiting ...",
+        "selectFlow":"请选择ProcessFlow。",
+        "gotoFlow":"移动到注册ProcessFlow",
+        "processing":"Progressing"
+    },
+    "factory": {
+        "factory": "Factory",
+        "area": "Area",
+        "subArea": "Sub Area",
+        "machine": "Machine",
+        "subMachine":"Sub Machine",
+        "port": "Port",
+        "factoryRelation":"Factory Relation",
+        "sourceFactory": "Source Factory",
+        "destinationFactory": "Destination Factory",
+        "root": "Root"
+    },
+    "stateModel": {
+        "leftTitle": "StateModel",
+        "rightTitle" : "StateModelState",
+        "rigthDownTitle" : "StateModelEvent"
+    },
+    "recipe":{
+        "title" : "Recipe",
+        "recipeNameSpace": "RecipeNameSpace",
+        "recipeParameter": "RecipeParameter",
+        "recipeParameterDefinition": "RecipeParameterDefinition",
+        "definitonButton": "Recipe Parameter Definition",
+        "posPolicyButton": "POSPolicy"
+},
+    "reasonCode": {
+        "title" : "ReasonCode"
+    },
+    "machineGroup": {
+        "title" : "Machine Group"
+    },
+    "machineStateGroup": {
+        "title" : "Machine State Group"
+    },
+    "machineCapabilityDef": {
+        "title" : "Machine Capability Def"
+    },
+    "machineCapability": {
+        "machineCapability" : "Machine Capability",
+        "chamberCapability" : "Chamber Capability"
+    },
+    "productSpec": {
+        "title": "ProductSpec",
+        "selectProductSpec" : "Selected ProductSpec",
+        "possibleProcessFlow": "Possible ProcessFlow",
+        "selectedProcessFlow": "Selected ProcessFlow"
+    },
+    "consumableSpec": {
+        "title": "ConsumableSpec"
+    },
+    "durableSpec": {
+        "title": "DurableSpec"
+    },
+    "productMaterialType":{
+        "title": "Product MaterialType"
+    },
+    "reticleType":{
+        "title": "ReticleType"
+    },
+    "reticleSpec":{
+        "reticleSet": "ReticleSet",
+        "reticleGroup": "ReticleGroup",
+        "reticleAssociation": "ReticleAssociation"
+    },
+    "processOperationSpec":{
+        "title": "ProcessOperationSpec"
+    },
+    "processFlow": {
+        "title": "ProcessFlow"
+    },
+    "processSpec": {
+        "title": "ProcessSpec"
+    },
+    "dcSpec": {
+        "dcItem": "DCItem",
+        "dcSite": "DCSite",
+        "dcSpec": "DCSpec",
+        "dcSpecItem": "DCSpecItem",
+        "dcSpecDerivedItem": "DCSpecDerivedItem"
+    },
+    "spcHoldCondition": {
+        "title": "SPC Hold Condition"
+    },
+    "stage": {
+        "title": "Stage"
+    },
+    "sorterCapability": {
+        "title": "SorterCapability"
+    },
+    "userSection": {
+        "title": "UserSection"
+    },
+    "foupContamination": {
+        "title": "FoupContamination"
+    },
+    "sorterContamination": {
+        "title": "Sorter Lot Contamination"
+    },
+    "alarmDef" : {
+        "title": "AlarmDef",
+        "alarmType": "AlarmType",
+        "alarmDefinition": "Alarm Definition",
+        "alarmActionSetup": "Alarm Action Setup",
+        "action": "Action",
+        "reasonCodeBy": "ReasonCode by",
+        "holdDescription": "Hold Description",
+        "detailType": "Detail Type",
+        "duration": "Duration",
+        "informType": "InformType",
+        "boundary": "Boundary",
+        "noDataToApply": "没有数据可以更改。"
+    },
+    "machineAlarmDef" : {
+        "title": "MachineAlarmDef"
+    },
+    "objectOwnerDef" :{
+        "title": "ObjectOwnerDef"
+    },
+    "nameGeneratorRuleDef": {
+        "title": "NameGeneratorRuleDef"
+    },
+    "nameGeneratorRuleAttrDef": {
+        "title": "NameGeneratorRuleAttrDef"
+    },
+    "objectTypeDef": {
+        "title": "ObjectTypeDef",
+        "warningCheckFlag": "因为Flag为 N，所以无法使用。",
+        "warningCheckOperationData": "ObjectOperationDef 请先添加数据"
+    },
+    "objectBehavior": {
+        "title": "ObjectBehavior",
+        "policy": "Policy",
+        "validation": "Validation"
+    },
+    "objectBehaviorView": {
+        "title": "ObjectBehaviorView"
+    },
+    "objectAttributeDef": {
+        "title": "ObjectAttributeDef",
+        "standard": "Standard",
+        "extendedC": "ExtendedC",
+        "extendedT": "ExtendedT"
+    },
+    "objectOperationDef": {
+        "title": "ObjectOperationDef",
+        "objectPolicyDef": "ObjectPolicyDef",
+        "objectValidationDef": "ObjectValidationDef",
+        "objectEventDef": "ObjectEventDef"
+    },
+    "posPolicyDef": {
+        "posConditionDef": "POSConditionDef",
+        "posConditionAttributeDef": "POSConditionAttributeDef",
+        "posPolicyDef": "POSPolicyDef",
+        "posPolicyAttributeDef": "POSPolicyAttributeDef",
+        "posPolicyHistory": "POSPolicyHistory",
+        "posPolicyConditionDef": "POSPolicyConditionDef",
+        "posPolicyConditionDefView": "POSPolicyConditionDef (VIEW)",
+        "posPolicyConditionDefSearch": "POSPolicyConditionDef (SEARCH)",
+        "accessTypeCondition" : "连接类型 0: 增加 / 连接类型 1: 增加, 修改, 删除",
+        "errorModifyCondition" : "请选择连接类型为 1 的 data。",
+        "errorDeleteCondition" : "请选择连接类型为 1的 data。",
+        "noData" : "Not exist policy data.",
+        "notExistHistoryName" : "因为History Table 名为空，所以无法增加 data。"
+    },
+    "enumDef" : {
+        "title": "EnumDef",
+        "errorModifyCondition" : "请选择连接类型为 1 或是 2的其他  EnumDef data。",
+        "errorDeleteCondition" : "[Access type 0, 1 does not have permission to delete.] <br/> <font style='font-size:120%;'>请选择连接类型为 2的 data。</font>"
+    },
+    "enumDefValue" : {
+        "title": "EnumDefValue",
+        "accessTypeCondition" : "连接类型 0: 修改 / 连接类型 1: 增加, 修改 / 连接类型 2: 增加, 修改, 删除",
+        "errorCreateCondition" : "请选择连接类型为 1或是 2的其他 EnumDef data。",
+        "errorDeleteCondition" : "请选择连接类型为 2的 data。"
+    },
+    "constantDef" : {
+        "title": "ConstantDef"
+    },
+    "gradeDefinition" : {
+        "title": "GradeDefinition"
+    },
+    "gridDesignDef" : {
+        "title": "GridDesignDef"
+    },
+    "gridDesignDefValue" : {
+        "title": "GridDesignDefValue"
+    },
+    "sqlEditor" : {
+        "title": "SqlEditor"
+    },
+    "sqlViewer" : {
+        "title": "SqlViewer"
+    },
+    "menu": {
+        "title": "Menu",
+        "systemUI": "System UI",
+        "menuRelation": "Menu Relation",
+        "relations": "Related Menu",
+        "check": "Check",
+        "clear": "Clear",
+        "apply": "Apply",
+        "menuInform": "Menu Information"
+    },
+    "user": {
+        "leftTitle": "User Group",
+        "rightTitle": "User Profile",
+        "userGroupMap": "User Group Map",
+        "showAllUserProfile": "All User Profile"
+    },
+    "userMenu": {
+        "write": "Write",
+        "read": "Read",
+        "clear": "Clear",
+        "disable": "Disable",
+        "apply": "Apply",
+        "create": "Create",
+        "modify": "Modify",
+        "delete": "Delete",
+        "success": "Success",
+        "fail": "Fail",
+        "noDataToApply": "没有数据可以更改权限。"
+    },
+    "tfsPolicy": {
+        "title": "TFSPolicy",
+        "factoryName": "FactoryName",
+        "mainFlowType": "MainFlowType",
+        "processFlowName": "ProcessFlowName",
+        "processFlowVersion": "ProcessFlowVersion",
+        "posReference":"POSReference"
+    },
+    "tufsPolicy": {
+        "title": "TUFSPolicy",
+        "sequenceTitle": "TUFSPolicySequence",
+        "factoryName": "FactoryName",
+        "processSpecName": "ProcessSpecName",
+        "processSpecVersion": "ProcessSpecVersion",
+        "posQueueTime":"POSQueueTime",
+        "posCriticalLayer":"POSCriticalLayer"
+    },
+    "common": {
+        "id":"ID",
+        "name":"Name",
+        "version":"Version",
+        "type":"Type",
+        "custom":"Custom",
+        "factoryName":"FactoryName",
+        "information": "Information",
+        "history": "History",
+        "attributeHistory": "AttributeHistory",
+        "dragDropInfo": "Drag and drop 这个 grid 之间可行。",
+        "dragDropGrid": "Drag and drop 这个 grid 之内可行。"
+    },
+    "button": {
+        "modify":"Modify",
+        "save":"Save",
+        "open":"Open",
+        "close": "Close",
+        "add": "Add",
+        "ok": "Ok",
+        "checkedIn":"Save(CheckedIn)",
+        "checkedOut":"Edit(CheckedOut)",
+        "view":"View",
+        "delete":"Delete",
+        "set": "Set",
+        "apply": "Apply",
+        "search": "Search",
+        "active": "Active",
+        "activate": "Activate",
+        "deactivate": "NotActive",
+        "clone": "MakeClone",
+        "newVersion": "New Version",
+        "possibleProcessFlow": "Possible ProcessFlow",
+        "reset": "Reset",
+        "changeData": "Change Data",
+        "addData": "Add Data",
+        "recall": "Recall",
+        "selectAll": "Select All",
+        "deselectAll": "Deselect All",
+        "saveAll": "Save All",
+        "importExcel":"ImportExcel",
+        "exportExcel":"ExportExcel",
+        "yes": "Yes",
+        "no": "No",
+        "goToPolicy": "POSPolicy",
+        "cancel": "Cancel",
+        "clear": "Clear",
+        "update": "Update",
+        "rollUp": "RollUp"
+    },
+    "modal":{
+        "changeLanguage": {
+            "language": "Language"
+        },
+        "clone": {
+            "title": "MakeClone",
+            "comment": "请输入要复制的名字。",
+            "existData": "The data already exists"
+        },
+        "create": {
+            "title": "Create List",
+            "key": "Key",
+            "value": "Value",
+            "valueUdfs": "Value Udfs",
+            "gridTitle": "New Add List View",
+            "confirmTitle": "Add",
+            "requiredKey": "请输入PrimaryKey 值。",
+            "mandatoryFlag": "请输入MandatoryFlag 值。",
+            "existData": "当前增加的 data 已存在。",
+            "noData": "No data to add",
+            "success": "Success",
+            "fail": "Fail",
+            "condition": "Condition",
+            "policy": "Policy: ",
+            "noDataToDelete": "没有要删除的 data。",
+            "checkDataOfColumn": "The value of {column} must be [{value}].",
+            "diffKey": "Different {column} exist."
+        },
+        "modify": {
+            "title": "Modify List",
+            "panelTitle": "Modify items Select",
+            "gridTitle": "Modify List View",
+            "confirmTitle": "Modify",
+            "dependentValue": "请选择附属的值。",
+            "success": "Success",
+            "fail": "Fail",
+            "associateField": "当前所选 {param} 与 {param2} 有关联。",
+            "noDataToModify": "没有可删除的数据",
+            "impossibleToDelete": "It can no longer be deleted."
+        },
+        "confirm": {
+            "title": "Confirm",
+            "register": "新注册[{count}]个的 {name} 。",
+            "modify": "修改[{count}]个 {name} data。",
+            "delete": "删除[{count}]个的 {name} 。",
+            "apply": "Apply {attributeName}.",
+            "closeAll": "Do you want to close all tabs?"
+        },
+        "validation": {
+            "title" : "Validation"
+        },
+        "summaryView" :{
+            "title" : "Summary View",
+            "stateDataEmpty" : "State Data 为空。"
+        },
+        "apply": {
+            "confirmTitle": "Apply"
+        },
+        "policyCopy": {
+            "confirmTitle": "Confirm",
+            "comment": "Policy也要一起复制吗?"
+        },
+        "specValidation": {
+            "title": "Spec Validation",
+            "objectType": "ObjectType"
+        },
+        "search": {
+            "title": "Search",
+            "result": "Result"
+        }
+    },
+    "info": {
+        "title": {
+            "success":"Success",
+            "emptyData": "Empty data"
+        },
+        "success":"Success",
+        "info": "Info",
+        "syncSuccess": "Synchronization succeeded",
+        "emptyData": "{param} enter an empty value",
+        "successDelete": "Success delete.",
+        "initialization": "{param} is initialized",
+        "applied": "Applied."
+    },
+    "warning": {
+        "title": {
+            "validation":"Check Diagram",
+            "loseFile":"Lose File",
+            "validateCheckedOut":"Edit(CheckedOut)",
+            "enterItem":"Enter Item"
+        },
+        "startNodeNone": "没有开始Node。",
+        "startNodeTooMuch": "开始Node过多。",
+        "endNodeNone": "没有结束Node。",
+        "endNodeTooMuch": "结束Node过多。",
+        "startNodeArcNone":"没有从开始Node出去的Arc。",
+        "endNodeArcNone":"没有从结束Node进来的Arc",
+        "incomingArcNone":" 没有进入到Node的Arc。",
+        "outgoingArcNone":" 没有从Node出去的Arc。",
+        "sourceNodeNone":" 无source Node。",
+        "targetNodeNone":" 无目标 Node。",
+        "selectData": "请选择数据。",
+        "selectOneData": "请选择一个数据。",
+        "limitCheckCount": "无法选择20个以上。",
+        "enterItem": "请输入{param} 。", 
+        "loseFile":"请进行 CheckedIn。目前作业中的文件未存储。",
+        "validateCheckedOut":"Please CheckedOut.",
+        "requiredField" : "{param} 为必选值。",
+        "noMatching" : "No matching values.",
+        "notSameFactories": "相同 Factory 之间才能修改。",
+        "selectFirstData": "请先选择{param} 。",
+        "writeMemo": "请输入内容。",
+        "selectParam": "请选择 {param}",
+        "nodeSelect": "请在树中选择 {param} 节点。",
+        "notMatchCheckOutUser": "CheckOut user not matched."
+    },
+    "error": {
+        "title": {
+            "error": "Error",
+            "dataEmpty":"Data Empty",
+            "dataLoad":"Data Load",
+            "setData":"Set Data",
+            "userIdEmpty":"User Id Empty",
+            "userPwEmpty":"User Password Empty",
+            "loginFailure": "Login Failure",
+            "showDiagramFail":"Show Diagram Fail",
+            "drawNodeFail":"Draw Node Fail",
+            "invalidUserInfo":"Invalid User Info",
+            "saveFileFail":"Save File Fail",
+            "validationFail": "Validation Fail"
+        },
+        "dataEmpty": "无Data。请确认服务器。",
+        "fileEmpty": "文件为空。请确认服务器。",
+        "userIdEmpty":"请输入ID。",
+        "userPwEmpty":"请输入密码。",
+        "loginFailure": "登录失败。请确认用户ID/密码。",
+        "impossibleActiveState": "因所选 Data不是 {param} ，所以不行。",
+        "sameNode":"开始、结束 Node一样。请选择其他Node。",
+        "saveFileFail":"存储失败。",
+        "excel" : {
+            "columnEmpty": "Column Data 为空。",
+            "duplicateData": "存在重复 Data。"
+        },
+        "changeAuthority": {
+            "goMain" : "權限已更改，您將被移至主屏幕。",
+            "inaccessible" : "您無權訪問{param}菜單。"
+        }
+    }
+}
+
+function log() {
+    for (const [key1, value1] of Object.entries(obj)) {
+        if (typeof value1 === "object") {
+            for (const [key2, value2] of Object.entries(value1)) {
+                if (typeof value2 === "object") {
+                    for (const [key3, value3] of Object.entries(value2)) {
+                        // console.log(`${key1}.${key2}.${key3}`);
+                        console.log(encodeResourceValue(value3));
+                    }
+                } else {
+                    // console.log(`${key1}.${key2}`);
+                    console.log(encodeResourceValue(value2));
+                }
+            }
+        } else {
+            // console.log(`${key1}`);
+            console.log(encodeResourceValue(value1));
+        }
+    }
+}
+
+function encodeResourceValue(value) {
+    let unicode = "";
+    for (let i of value) {
+        let charCode = i.charCodeAt(0).toString(16);
+        unicode += `\\u${charCode.padStart(4, "0")}`;
+    }
+
+    return unicode;
+}
+
+log();
