d9f8a981e63e6cbbd3f130e8b05fc0e19d9941e4
umingming <u_0112@naver.com>
2023-04-10 09:18:23 +0900

    feat: Apply korean flag

--- vo/encodeData.js
+++ vo/encodeData.js
@@ -1,53 +1,122 @@
-const str = `CauseMainProcessFlowName: 원인 주요 프로세스 흐름 이름
-CauseMainProcessFlowVersion: 원인 주요 프로세스 흐름 버전
-CauseStepSequence: 원인 단계 시퀀스
-CenterLine: 중심선
-CenterLine2: 중심선2
-CenterLint: 중앙 선
-Cha_ErrorMsg: 에러 메시지
-ChamberCondition: 챔버 상태
-ChamberInTime: 챔버 진입 시간
-ChamberName: 챔버 이름
-ChamberOutTime: 챔버 이탈 시간
-ChamberRecipePriority: 챔버 레시피 우선순위
-ChannelId: 채널 ID
-ChartName: 차트 이름
-Check Privilege Flag: 권한 검사 플래그
-Check Spec Name: 검사 스펙 이름
-CheckOutTime: 체크아웃 시간
-CheckOutUser: 체크아웃 사용자
-CheckSpecName: 검사 스펙 이름
-CheckState: 체크 상태
-Chinese: 중국어
-ChuckCount: 척 개수
-ChuckName: 척 이름
-CleanCount: 청소 횟수
-CleanCountLimit: 청소 횟수 제한
-ClientId: 클라이언트 ID
-ClientVersion: 클라이언트 버전
-CmpRecipeName: Cmp 레시피 이름
-ColumnName: 열 이름
-ColumnStyle: 열 스타일
-CommunicationState: 통신 상태
-CompleteTime: 완료 시간
-CompleteUser: 완료 사용자
-Condition: 조건
-ConditionID: 조건 ID
-ConditionId: 조건 ID
-ConditionName: 조건 이름
-ConfirmType: 확인 유형
-ConstantFlag: 상수 플래그
-ConstantName: 상수 이름
-ConstantValue: 상수 값
-ConstructType: 구성 유형
-ConsumableName: 소모품 이름
-ConsumableSpecName: 소모품 스펙 이름
-ConsumableSpecVersion: 소모품 스펙 버전`
+const str = `ProdFoupQty
+제품 Foup 수량
+ProductFlowName
+제품 Flow 이름
+ProductFlowVersion
+제품 Flow 버전
+ProductGrade
+제품 등급
+ProductGroupFlag
+제품 그룹 플래그
+ProductHoldState
+제품 보류 상태
+ProductName
+제품 이름
+ProductOwner
+제품 소유자
+ProductProcessState
+제품 공정 상태
+ProductQuantity
+제품 수량
+ProductRequestGrade
+제품 요청 등급
+ProductRequestHoldState
+제품 요청 보류 상태
+ProductRequestName
+제품 요청 이름
+ProductRequestState
+제품 요청 상태
+ProductRequestType
+제품 요청 타입
+ProductSpec
+제품 스펙
+ProductSpec2Name
+제품 스펙2 이름
+ProductSpec2Version
+제품 스펙2 버전
+ProductSpecGroupName
+제품 스펙 그룹 이름
+ProductSpecName
+제품 스펙 이름
+ProductSpecVersion
+제품 스펙 버전
+ProductState
+제품 상태
+ProductType
+제품 타입
+ProductionOrderName
+생산 오더 이름
+ProductionType
+생산 타입
+Quantity
+수량
+QuantityFlag
+수량 플래그
+QueryId
+쿼리 아이디
+QueryString
+쿼리 문자열
+QueueTimeDuration
+대기 시간
+QueueType
+대기열 타입
+R2RDATA
+R2R 데이터
+R2RFlag
+R2R 플래그
+RCConditionId
+RC 상태 아이디
+RCJobState
+RC 작업 상태
+RCMachineCapabilityName
+RC 기계 능력 이름
+REASONCODE
+사유 코드
+REQUESTID
+요청 아이디
+REQUESTTYPE
+요청 타입
+RFID
+RFID
+RMSFlag
+RMS 플래그
+RSPName
+응답 이름
+RackName
+랙 이름
+RackQty
+랙 수량
+RackType
+랙 타입
+Range
+범위
+RawWaferId
+기판 아이디
+ReMark
+비고
+ReadOnly
+읽기 전용
+ReasonCode
+사유 코드
+ReasonCodeFlag
+사유 코드 플래그
+ReasonCodeGroup
+사유 코드 그룹
+ReasonCodeType
+사유 코드 타입
+ReasonCodeTypeName
+사유 코드 타입 이름`
 
+let korean = false;
 function log() {
     for (const i of str.split("\n")) {
-        const value = i.split(": ")[1];
-        console.log(encodeValue(value));
+        if (!korean) {
+            console.log(i);
+        }
+        korean = !korean;
+        // const value = i.split(": ")[1];
+        // console.log(encodeValue(i));
+        // console.log(i.split(": ")[0]);
     }
 }
 function encodeValue(value) {
