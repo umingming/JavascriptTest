4aaddd46ddedd3120b96c1bc289ea1d8f65fae80
umingming <u_0112@naver.com>
2023-04-14 14:06:59 +0900

    feat: Encode data

--- vo/encodeData.js
+++ vo/encodeData.js
@@ -1,122 +1,297 @@
-const str = `ProdFoupQty
-제품 Foup 수량
-ProductFlowName
-제품 Flow 이름
-ProductFlowVersion
-제품 Flow 버전
-ProductGrade
-제품 등급
-ProductGroupFlag
-제품 그룹 플래그
-ProductHoldState
-제품 보류 상태
-ProductName
-제품 이름
-ProductOwner
-제품 소유자
-ProductProcessState
-제품 공정 상태
-ProductQuantity
-제품 수량
-ProductRequestGrade
-제품 요청 등급
-ProductRequestHoldState
-제품 요청 보류 상태
-ProductRequestName
-제품 요청 이름
-ProductRequestState
-제품 요청 상태
-ProductRequestType
-제품 요청 타입
-ProductSpec
-제품 스펙
-ProductSpec2Name
-제품 스펙2 이름
-ProductSpec2Version
-제품 스펙2 버전
-ProductSpecGroupName
-제품 스펙 그룹 이름
-ProductSpecName
-제품 스펙 이름
-ProductSpecVersion
-제품 스펙 버전
-ProductState
-제품 상태
-ProductType
-제품 타입
-ProductionOrderName
-생산 오더 이름
-ProductionType
-생산 타입
-Quantity
-수량
-QuantityFlag
-수량 플래그
-QueryId
-쿼리 아이디
-QueryString
-쿼리 문자열
-QueueTimeDuration
-대기 시간
-QueueType
-대기열 타입
-R2RDATA
-R2R 데이터
-R2RFlag
-R2R 플래그
-RCConditionId
-RC 상태 아이디
-RCJobState
-RC 작업 상태
-RCMachineCapabilityName
-RC 기계 능력 이름
-REASONCODE
-사유 코드
-REQUESTID
-요청 아이디
-REQUESTTYPE
-요청 타입
-RFID
-RFID
-RMSFlag
-RMS 플래그
-RSPName
-응답 이름
-RackName
-랙 이름
-RackQty
-랙 수량
-RackType
-랙 타입
-Range
-범위
-RawWaferId
-기판 아이디
-ReMark
-비고
-ReadOnly
-읽기 전용
-ReasonCode
-사유 코드
-ReasonCodeFlag
-사유 코드 플래그
-ReasonCodeGroup
-사유 코드 그룹
-ReasonCodeType
-사유 코드 타입
-ReasonCodeTypeName
-사유 코드 타입 이름`
+const str = `레티클 그룹 이름
+레티클 홀드 상태
+레티클 이름
+레티클 공정 상태
+레티클세트
+레티클 세트 이름
+레티클 상태
+레티클 유형
+레티클 유형 이름
+반환 코드
+리턴 플로우 이름
+리턴 플로우 버전
+리턴 플로우 설명
+반환 작업 이름
+리턴 작업 버전
+리턴 단계 순서
+리턴 단계 순서
+검토 유형
+재작업 횟수
+재작업 횟수 제한
+재작업 플로우 이름
+재작업 플로우 버전
+재작업 노드 아이디
+재작업 공정 이름
+재작업 작업 버전
+재작업 상태
+재작업 단계 시퀀스
+루트 지역 이름
+루트 랏 이름
+Rsp
+규칙 이름
+런카드 ID
+런카드 메인 ID
+소스머신이름
+소스 포트 이름
+소스존이름
+SPC 제어 규칙 그룹 이름
+SPC 제어 규칙 이름
+SPC 제어 사양 이름
+SPC 제한 이름
+SPCRequired플래그
+SP 측정 횟수
+SP 측정 횟수 제한
+안전가치
+판매 주문
+샘플 제어 갯수
+샘플 갯수
+샘플 로트 유형
+샘플 재료 이름
+샘플 재료 유형
+샘플번호
+샘플링 플래그
+샘플링 그룹
+샘플링 그룹 유형
+샘플링 사용자 그룹
+ScanEven
+스캔 로트 목록
+스캔 로트 이름 규칙
+스캔 류트 유형
+홀수 스캔
+예정된 다운 타임 시작 시간
+스크랩 원인 날짜
+스크랩코드
+스크랩포지션
+스크랩제품명
+스크랩 수량
+스크랩타임
+폐기 수량
+스크린인
+스크린 제한 제거 옵션
+검색 위치
+시즌작업
+시즌로트
+부분
+섹션 카테고리
+단면 길이
+섹션 이름
+섹션 유형
+섹션 값
+선택 데이터
+규칙 선택
+순서
+시퀀스 순서
+일련 번호
+근무 종료 시간
+시프트종료분
+교대 이름
+근무 시작 시간
+시프트시작분
+시프트타임1
+시프트타임2
+시프트타임3
+출하 뱅크 ID
+출하 코드
+사이트 수
+사이트 이름
+사이트 이름
+건너뛰기 허용됨
+LotList 건너뛰기
+최대 대기 시간 한계
+건너뛰기WaferCountLimit
+분류기 작업 ID
+분류기 이름
+소스 캐리어 네임
+소스 팩토리 네임
+소스 랏 네임
+소스 모니터 랏 네임
+소스제품명
+소스슬롯아니요
+스패어 파트 네임
+스페어 파트 사양 네임
+스페어 파트 사양 버전
+예비 부품 상태
+Spc 결과
+사양 한계 유형
+분할 위치
+제품명 분할
+후원자 이름
+후원전화
+소스 Foup 공정 유형
+StDev
+단계
+공정 진입 시간
+예명
+공정 재작업 카운트
+시작 Bank ID
+시작 웨이퍼 위치
+상태
+상태 모델
+상태 모델 이벤트
+상태 모델 이름
+상태모델상태
+주 이름
+성병
+표준 공정 시간12
+표준 공정 시간25
+표준시
+단계 약어
+스텝시퀀스
+스테퍼머신 이름
+스테퍼 모델
+재고 보관 시간
+하위 영역
+하위 흐름 이름
+서브머신
+서브 제품 단계1
+서브 제품 단계2
+하위 제품 수량
+하위 제품수량1
+하위 제품 수량2
+하위 제품 유형
+하위 제품 단위수량1
+서브 제품 단위 수량2
+주제 이름
+수퍼 영억 이름
+수퍼머신 이름
+수퍼메뉴 이름
+수퍼 프로세스 그룹 이름
+슈퍼 사유 코드
+슈퍼 운송 그룹 이름
+감독자 플래그
+체계
+시스템 이름
+시스템 설정
+시스템 시간
+표적
+TBMP 기간
+TDUL 알람 ID
+TDUL 알람 ID
+처리 대상 공정 이름
+처리 대상 공정 버전
+이전 상태
+TR값
+TTU 알람 ID
+뚜라라미드
+TUL 알람 ID
+표적
+대상경계
+대상 메시지
+대상 유형
+목표치
+태스크 이름
+기술 카테고리
+테크노드
+전화1
+전화2
+전화3
+두께주의
+두께 제한
+타임키
+시간 설정
+사용 시간
+시간 사용 제한
+제목
+대상 Bank ID
+대상 이벤트 네임
+대상 랏 오염
+대상 메인 PF 이름
+대상 메인 PF 버전
+메뉴로
+대상 노드 아이디
+대상 PO 설명
+대상 PO 이름
+대상 공정 사양 이름
+대상 공정 사양 버전
+대상 레시피
+단계 시퀀스로
+용인
+총 사용 가능 시간 제한
+총 사용 시간
+총 사용 가능 시간 제한
+총 제어 수
+총 사용 가능 시간 제한
+총 사용 시간 제한
+총 길이
+사용된 총 시간
+TotalTimeUsedLimit
+트랙아웃 시간
+TransferState
+전송 속도 제한
+운송 그룹 이름
+TransportGroupType
+유형 이름
+UDFS01
+UIName
+사용자 ID
+사용자 이름
+UiName
+측정 단위
+예약되지 않은 다운시작 시간
+업데이트 시간
+사용자 업데이트
+UpperControlLimit
+UpperControlLimit2
+상한
+상한 품질 제한
+UpperScreenLimit
+UpperScreenLimit(상위 특이치)
+사양 상한
+어퍼TRLimit
+UpperWaferCountLimit
+용법
+사용횟수주의
+UsageCountLimit
+사용법플래그
+EDCFlag 사용
+사용자
+사용자 아이디
+UserActionFlag
+사용자 코멘트
+사용자 의견
+사용자 그룹 이름
+사용자 ID
+사용자 ID
+사용자 이름
+유효성 검사 이름
+유효성 검사 유형
+값
+가치 단위
+공급업체
+VendorLotName
+버전
+보기위치
+보이는
+WaferCountLowerLimit
+웨이퍼 ID 목록
+웨이퍼 위치
+웨이퍼선택플래그
+WaferSelectedId
+WaferSelectedIdNew
+WaferSelectedUser
+웨이퍼 유형
+WaferVendorCode
+경고 기간 제한
+경고 ID
+경고ProcCountLimit
+WarningTimeDuration
+올해의 주
+너비
+WithHistoryFlag
+작업 공간
+근무일플래그
+작업 상태
+워크스테이션 이름
+엑스
+X좌표
+Y좌표
+년도`
 
-let korean = false;
 function log() {
     for (const i of str.split("\n")) {
-        if (!korean) {
-            console.log(i);
-        }
-        korean = !korean;
-        // const value = i.split(": ")[1];
-        // console.log(encodeValue(i));
-        // console.log(i.split(": ")[0]);
+        // const value = i.split(" : ")[1];
+        console.log(encodeValue(i));
+        // console.log(i.split(" : ")[0]);
     }
 }
 function encodeValue(value) {
