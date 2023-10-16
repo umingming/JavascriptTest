fde20c3e151d7dfc35a854bb41c2e94b0205e4eb
umingming <u_0112@naver.com>
2023-10-16 11:35:17 +0900

    Test performance for array

--- array/27-09.js
+++ array/27-09.js
@@ -1,1 +1,15 @@
-console.log(Object.getOwnPropertyDescriptors(["string", 2, 3, null, true, [], {}]));
+// console.log(Object.getOwnPropertyDescriptors(["string", 2, 3, null, true, [], {}]));
+
+const arr = [];
+console.time("Array Performance Test");
+for(let i = 0; i < 100000000; i++) {
+    arr[i] = i;
+}
+console.timeEnd("Array Performance Test");
+
+const obj = {};
+console.time("Object Performance Test");
+for(let i = 0; i < 100000000; i++) {
+    obj[i] = i;
+}
+console.timeEnd("Object Performance Test");
