213f0b5658604d6bb305443ce8dd184503f39f3b
umingming <u_0112@naver.com>
2023-10-16 12:20:46 +0900

    Implement a stack using Class

--- array/27-50.js
+++ array/27-50.js
@@ -1,24 +1,52 @@
-const Stack = (function () {
-    function Stack(array = []) {
+// const Stack = (function () {
+//     function Stack(array = []) {
+//         if(!Array.isArray(array)) {
+//             throw new TypeError(`${array} is not an array`)
+//         }
+//         this.array = array;
+//     }
+//     Stack.prototype = {
+//         constructor: Stack,
+//         push(value) {
+//             return this.array.push(value);
+//         },
+//         pop() {
+//             return this.array.pop();
+//         },
+//         entries() {
+//             return [...this.array];
+//         }
+//     };
+//     return Stack;
+// }());
+
+// const stack = new Stack([1, 2]);
+// console.log(stack.entries());
+// stack.push(3);
+// stack.push(4);
+// stack.pop();
+// console.log(stack.entries());
+
+class Stack {
+    #array;
+    
+    constructor(array = []) {
         if(!Array.isArray(array)) {
             throw new TypeError(`${array} is not an array`)
         }
-        this.array = array;
+        this.#array = array;
     }
-    Stack.prototype = {
-        constructor: Stack,
-        push(value) {
-            return this.array.push(value);
-        },
-        pop() {
-            return this.array.pop();
-        },
-        entries() {
-            return [...this.array];
-        }
-    };
-    return Stack;
-}());
+
+    push(value) {
+        return this.#array.push(value)
+    }
+    pop() {
+        return this.#array.pop();
+    }
+    entries() {
+        return [...this.#array];
+    }
+}
 
 const stack = new Stack([1, 2]);
 console.log(stack.entries());
