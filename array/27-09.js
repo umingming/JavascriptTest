ce248d7810f25a559fc841a5186fb94a94251baa
umingming <u_0112@naver.com>
2023-10-16 11:40:37 +0900

    feat: Check array elements

--- array/27-09.js
+++ array/27-09.js
@@ -1,15 +1,24 @@
 // console.log(Object.getOwnPropertyDescriptors(["string", 2, 3, null, true, [], {}]));
 
-const arr = [];
-console.time("Array Performance Test");
-for(let i = 0; i < 100000000; i++) {
-    arr[i] = i;
-}
-console.timeEnd("Array Performance Test");
+// const arr = [];
+// console.time("Array Performance Test");
+// for(let i = 0; i < 100000000; i++) {
+//     arr[i] = i;
+// }
+// console.timeEnd("Array Performance Test");
 
-const obj = {};
-console.time("Object Performance Test");
-for(let i = 0; i < 100000000; i++) {
-    obj[i] = i;
-}
-console.timeEnd("Object Performance Test");
+// const obj = {};
+// console.time("Object Performance Test");
+// for(let i = 0; i < 100000000; i++) {
+//     obj[i] = i;
+// }
+// console.timeEnd("Object Performance Test");
+
+
+const arr = [1, 2, 5, 3];
+arr.length = 3;
+console.log(arr);
+arr.length = 6;
+console.log(arr);
+console.log(arr.length);
+console.log(Object.getOwnPropertyDescriptors(arr));
