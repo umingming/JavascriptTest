b466f044b323a8b8c79d098e9f5fd70d162ebc99
umingming <u_0112@naver.com>
2023-11-08 17:11:48 +0900

    create Array

--- array/27-72.js
+++ array/27-72.js
@@ -17,3 +17,31 @@ function sum2() {
 }
 
 console.log(sum2(1, 2, 3));
+
+function sum3() {
+    const arr = [ ...arguments ];
+    console.log(arr);
+
+    return arr.reduce((pre, cur) => pre + cur, 0);
+}
+
+console.log(sum3(1, 2, 3));
+
+const arr = [1, 2, 3];
+const result = arr.reverse();
+
+// console.log(arr);
+// console.log(result);
+
+// arr.fill(0);
+// console.log(arr);
+
+// arr.fill(1, 0, 1);
+// console.log(arr);
+
+// const seq = (length = 0) => Array.from({ length }, (_, i) => i);
+// console.log(seq(3));
+
+console.log(arr.includes(3, 1));
+
+console.log([NaN].indexOf(NaN));
