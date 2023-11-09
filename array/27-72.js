44ba03b8291e5644ca4d5410366d1248a8d3298d
umingming <u_0112@naver.com>
2023-11-09 14:50:04 +0900

    sort by key

--- array/27-72.js
+++ array/27-72.js
@@ -51,3 +51,19 @@ console.log(multipleArr.flat());
 console.log(multipleArr.flat().flat());
 console.log(multipleArr.flat(2));
 console.log(multipleArr.flat(Infinity));
+
+const todos = [
+    { id: 4, content: 'JavaScript' },
+    { id: 1, content: 'HTML' },
+    { id: 2, content: 'CSS' },
+];
+
+function compare(key) {
+    return (a, b) => (a[key] > b[key] ? 1 : (a[key] < b[key] ? -1 : 0));
+}
+
+todos.sort(compare('id'));
+console.log(todos);
+
+todos.sort(compare('content'));
+console.log(todos);
