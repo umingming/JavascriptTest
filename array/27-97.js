6c3591dfec3be2d5a8491fc0ad0bbee048196a13
umingming <u_0112@naver.com>
2023-11-14 15:16:40 +0900

    test array function

--- array/27-97.js
+++ array/27-97.js
@@ -28,3 +28,57 @@ Numbers.prototype.forEach = function (callback, thisArg) {
     }
 }
 numbers.forEach(i => console.log(i))
+
+const numbers2 = [1, 4, 9]
+const roots = numbers2.map(item => Math.sqrt(item));
+console.log("roots: ", roots);
+console.log("numbers: ", numbers2);
+
+numbers2.map((item, index, arr) => {
+    console.log(item, index, arr);
+    return item;
+})
+
+class Prefixer {
+    constructor(prefix) {
+        this.prefix = prefix;
+    }
+
+    add(arr) {
+        // return arr.map(function (item) {
+        //     return this.prefix + item;
+        // }, this);
+        return arr.map(item => this.prefix + item);
+    }
+}
+
+const prefixer = new Prefixer('-webkit-');
+console.log(prefixer.add(['transition', 'user-select']));
+
+const result = [1, 2, 3].filter((item, index, arr) => {
+    console.log(item, index, arr);
+    return item % 2;
+})
+
+console.log(result);
+
+class Users {
+    constructor() {
+        this.users = [
+            { id: 1, name: "이유미" },
+            { id: 2, name: "엄윤섭" }
+        ]
+    }
+    findById(id) {
+        return this.users.filter(user => user.id === id);
+    }
+    remove(id) {
+        this.users = this.users.filter(user => user.id !== id);
+    }
+}
+
+const users = new Users();
+const user = users.findById(1);
+console.log(user);
+users.remove(1);
+console.log(users.findById(1));
