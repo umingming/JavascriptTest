a139101c7848caa5c333a9c4554442885ee5d5d5
umingming <u_0112@naver.com>
2024-01-17 14:13:51 +0900

    test number function

---
+++ number/28-21.js
@@ -0,0 +1,10 @@
+console.log(Number.isSafeInteger(100000000000000000000000000))
+
+console.log((88.123).toExponential())
+console.log((88.123).toExponential(2))
+console.log(88.123 .toExponential(2))
+console.log((88.183).toFixed(1))
+console.log((88.183).toPrecision())
+console.log((88.183).toPrecision(3))
+console.log((10).toString())
+console.log((10).toString(2))
