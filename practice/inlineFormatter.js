7cba690f89987366c2be1155a9a1ea0e3bc44a35
umingming <u_0112@naver.com>
2024-11-28 10:48:34 +0900

    feat: Validate inline style

--- practice/inlineFormatter.js
+++ practice/inlineFormatter.js
@@ -4,25 +4,71 @@
 */
 
 const style = `
-    .body {
-        display: block;
-        font-size: 18px;
-    }
-    .test {
-        display: block;
-        font-size: 20px;
-    }
-    .text-bold {
-        font-weight: 600;
-    }
+.container {
+    background: white;
+    border-radius: 20px;
+    box-shadow: 0px 8px 8px 0px rgba(34, 34, 34, 0.05), 0px 0px 8px 0px rgba(34, 34, 34, 0.05);
+    display: flex;
+    flex-direction: column;
+    gap: 40px;
+    padding: 50px;
+    padding-bottom: 40px;
+    width: 600px;
+}
+.header__logo {
+    height: 17px;
+}
+.main {
+    color: #777777;
+    font-size: 15px;
+    gap: 30px;
+    line-height: 26px; 
+}
+.main__title-heading {
+    color: #212126;
+    font-size: 24px;
+    line-height: 32px; 
+}
+.main__content {
+    gap: 40px;
+}
+.main__action-button {
+    background-color: #fa6e00;
+    border-radius: 10px;
+    border: 0;
+    color: white;
+    height: 60px;
+    padding-bottom: 20px;
+    padding-top: 20px;
+    width: 190px;
+}
+.footer {
+    align-items: flex-start;
+    align-self: stretch;
+    border-top: 1px solid #dddddd;
+    color: #999999;
+    font-size: 13px;
+    gap: 8px;
+    justify-content: flex-end;
+    line-height: 20px; 
+    padding-top: 20px;
+}
+.flex-column {
+    display: flex;
+    flex-direction: column;
+}
+.text-bold {
+    font-weight: 600;
+}
+.text-medium {
+    font-size: 18px;
+    line-height: 20px;
+}
 `;
 
 const body = `
-    <div class="body">
-        <div class="test text-bold">
-            확인
-        </div>
-    </div>
+<div class="container">
+</div>
 `;
 
 function parseStyleToConfig(str) {
@@ -40,14 +86,14 @@ function parseStyleToConfig(str) {
 
 function replaceInlineStyle(html) {
   const styleConfig = parseStyleToConfig(style);
-  const stylePattern = /class="([\w\s-]+)"/g;
+  const stylePattern = /[\s]class="([\w\s-]+)"/g;
 
   return html.replace(stylePattern, (_, classes) => {
     const inlineStyle = classes
       .split(" ")
       .map((cls) => styleConfig[cls] || "")
       .join(";");
-    return `style="${inlineStyle}"`;
+    return inlineStyle ? ` style="${inlineStyle}"` : "";
   });
 }
 
